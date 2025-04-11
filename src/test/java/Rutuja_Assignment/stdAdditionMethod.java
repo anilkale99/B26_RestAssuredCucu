83ea9c5b67b43e05c5f8e9df8a7f2657d59f2bed
Rutuja <rutuja.b721@gmail.com>
2025-04-11 16:16:32 +0530

    Add My sum code

---
+++ src/test/java/Rutuja_Assignment/stdAdditionMethod.java
@@ -0,0 +1,22 @@
+package moreAssignments;
+
+public class stdAdditionMethod {
+
+
+		    public static double add(Number a, Number b) {
+		        return a.doubleValue() + b.doubleValue();
+		    }
+
+		    public static void main(String[] args) {
+		    	
+		        System.out.println("Add int: " + add(5, 10));   
+		        System.out.println("Add float: " + add(5.5, 4.5));      
+		        System.out.println("Add double: " + add(2.2, 3.3));         
+		        System.out.println("Add mixed (int + float): " + add(3, 2.5)); 
+		        
+		    }
+
+	
+	
+
+}
