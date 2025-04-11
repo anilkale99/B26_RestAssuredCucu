adfd776740dcb4976c557217399b1de07cb3f2ac
unknown <prasad.more.jgd@gmail.com>
2025-04-10 23:19:59 -0700

    added new folder prasad runner files

---
+++ prasad_RunnerFiles/RunnerForGetDataTable.java
@@ -0,0 +1,22 @@
+package prasad_RunnerFiles;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import io.cucumber.junit.platform.engine.Constants;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("prasad_assignment_SD")
+@ConfigurationParameter(key = Constants.FEATURES_PROPERTY_NAME,value = "src/test/resources/prasad_assignment_FF/GetDataTable.feature")
+@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME,value = "prasad_assignment_SD")
+@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME,value = "pretty, html:target/cucumber-report/cucumber.html")
+@ConfigurationParameter(key = Constants.PARALLEL_CONFIG_FIXED_PARALLELISM_PROPERTY_NAME,value = "1")
+@ConfigurationParameter(key = Constants.PARALLEL_CONFIG_STRATEGY_PROPERTY_NAME,value = "fixed")
+@ConfigurationParameter(key = Constants.PARALLEL_EXECUTION_ENABLED_PROPERTY_NAME,value = "false")
+
+public class RunnerForGetDataTable {
+
+}
