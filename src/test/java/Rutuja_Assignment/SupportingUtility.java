d3686cda32925373ce8567ccfc9f4970623243f2
Rutuja <rutuja.b721@gmail.com>
2025-04-21 00:03:20 +0530

    update utility- java

---
+++ src/test/java/Rutuja_Assignment/SupportingUtility.java
@@ -0,0 +1,23 @@
+package Rutuja_Assignment;
+
+public class SupportingUtility {
+	
+	public static boolean isNumber(String str)
+	{
+		boolean result= false;
+		char[] chArray = str.toCharArray();
+		for(char c : chArray)
+		{
+			if(!Character.isDigit(c))
+			{
+				result = false;
+			}
+			else
+			{
+				result = true;
+			}
+		}
+		
+		return result;
+	}
+}
