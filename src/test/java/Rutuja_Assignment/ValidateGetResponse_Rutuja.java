2f15b0ce9d7141e309f797f7597dcdc2bbc73eb8
Rutuja <rutuja.b721@gmail.com>
2025-04-22 18:15:22 +0530

    Add Validate Get

--- src/test/java/Rutuja_Assignment/ValidateGetResponse_Rutuja.java
+++ src/test/java/Rutuja_Assignment/ValidateGetResponse_Rutuja.java
@@ -1,5 +1,5 @@
 package Rutuja_Assignment;
-
+//package get_SD;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
@@ -20,22 +20,22 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
-import Rutuja_Assignment.SupportingUtility;
+import utility.SupportingUtility;
 
-public class ValidateGetResponse {
+public class ValidateGetResponse_Rutuja {
 	
 	String url;
 	Response response = null;
 	Response responseFailure = null;
 	
-	@Given("Hit books API")
-	public void hit_books_API()
+	@Given("Hit books API_Rutuja")
+	public void hit_books_API_Rutuja()
 	{
 		response = RestAssured.given().get("https://demoqa.com/BookStore/v1/Books");	
 	}
 	
-	@Then("Validate API returns multiple books json")
-	public void validate_API_returns_multiple_books_json()
+	@Then("Validate API returns multiple books json_Rutuja")
+	public void validate_API_returns_multiple_books_json_Rutuja()
 	{
 //		System.out.println("All logs "+response.then().log().all());
 		int bookCount = response.jsonPath().getList("books").size();
@@ -43,31 +43,30 @@ public class ValidateGetResponse {
 		Assert.assertTrue("Failed-Multiple books not present", bookCount>1);		
 	}
 	
-	@Then("Validate attribute at json path {string} and its value {string}")
-	public void Validate_attribute_at_json_path(String jsonPathStr , String expvalue) {
+	@Then("Validate attribute at json path {string} and its value {string}_Rutuja")
+	public void Validate_attribute_at_json_path_Rutuja(String jsonPathStr , String expvalue) {
 	    String actAttibutValue = response.body().jsonPath().getString(jsonPathStr);
 	    System.out.println("actual atr from response = "+actAttibutValue);
 	    Assert.assertTrue("Failed- atr value Not matching", expvalue.equals(actAttibutValue));
 	}
 	
-	@Then("Verify {string} attribute have numbers only")
-	public void verify_isbn_attribute_have_numbers_only(String jsonPathStr)	{
+	@Then("Verify {string} attribute have numbers only_Rutuja")
+	public void verify_isbn_attribute_have_numbers_only_Rutuja(String jsonPathStr)	{
 		String actIsbn = response.body().jsonPath().getString(jsonPathStr);
 		System.out.println(actIsbn);
 		Assert.assertTrue("Failed- ISBN contains characters", SupportingUtility.isNumber(actIsbn));
 	}
 	
-	@Given("If end point is not correct")
-	public void if_end_point_is_not_correct() {
+	@Given("If end point is not correct_Rutuja")
+	public void if_end_point_is_not_correct_Rutuja() {
 		Response invalidUrl = RestAssured.given().get("https://demoqa.com/BookStore/v1/Books");
 		int statcode = invalidUrl.statusCode();
 		System.out.println("StatusCode is "+statcode);
-		Assert.assertTrue("Incorrect endpoint" , statcode==502);
-		
+		Assert.assertTrue("Incorrect endpoint" , statcode==200);
 	}
 
-	 @Then("Validate json path {string} and confirm that there are total {int} attributes present")
-     public void validate_json_path_and_confirm_that_there_are_total_attributes_present(String jsonPathStr,
+	 @Then("Validate json path {string} and confirm that there are total {int} attributes present_Rutuja")
+     public void validate_json_path_and_confirm_that_there_are_total_attributes_present_Rutuja(String jsonPathStr,
                      int expectedCountOfAttributes){
             
              	Map<String, String> bookMap = response.body().jsonPath().getJsonObject(jsonPathStr);
@@ -77,8 +76,8 @@ public class ValidateGetResponse {
 
      }
 
-	@Then("Verify {string} attribute have numbers")
-	public void page_attribute_should_be_a_number_not_a_character(String jsonPath1) {
+	@Then("Verify {string} attribute have numbers_Rutuja")
+	public void page_attribute_should_be_a_number_not_a_character_Rutuja(String jsonPath1) {
 		System.out.println("Response body: " + response.getBody().asString());
 	
 		Object value = response.jsonPath().get(jsonPath1);
@@ -92,8 +91,8 @@ public class ValidateGetResponse {
 	    }
 	}
 	
-	@Then("{string} attributes value should start with {string} and end with {string}")
-	public void website_attributes_value_should_shart_with_and_end_with(String website, String expStartUrl, String expEndUrl) {
+	@Then("{string} attributes value should start with {string} and end with {string}_Rutuja")
+	public void website_attributes_value_should_shart_with_and_end_with_Rutuja(String website, String expStartUrl, String expEndUrl) {
 		 List<Map<String, Object>> books = response.body().jsonPath().getList("books");
 		 for (Map<String, Object> book : books) {
 			 Object value = book.get(website);
@@ -109,8 +108,8 @@ public class ValidateGetResponse {
 	}
 
 
-	@Then("Verify all {int} books should not be blank with {int} attributes")
-	public void all_books_should_have_attributes(int expectedBookCount, int expectedAttributeCount) {
+	@Then("Verify all {int} books should not be blank with {int} attributes_Rutuja")
+	public void all_books_should_have_attributes_Rutuja(int expectedBookCount, int expectedAttributeCount) {
 	    List<Map<String, Object>> books = response.jsonPath().getList("books");
 
 	    Assert.assertEquals("Book count mismatch", expectedBookCount,  books.size());
@@ -123,8 +122,8 @@ public class ValidateGetResponse {
 	    System.out.println("All " + expectedBookCount + " books have " + expectedAttributeCount + " attributes.");
 	}
 
-	@Then("verify date format {string}")
-	 public void each_format_should_match(String expectedDateFormat) {
+	@Then("verify date format {string}_Rutuja")
+	 public void each_format_should_match_Rutuja(String expectedDateFormat) {
 		
 	   	List<String> publishedDates = response.jsonPath().getList("books.publish_date");
 	   
@@ -137,8 +136,8 @@ public class ValidateGetResponse {
 				Assert.assertTrue("Date format mismatch "+ date, isMatch);	
 }}
 	
-	@Then("Validate that {string} publisher has only {int} books namely {string} and {string}")
-    public void publisher_should_have_only_books_namely_and(String publisherName, int expBooksCountForPublisher, String expBookTitle1,
+	@Then("Validate that {string} publisher has only {int} books namely {string} and {string}_Rutuja")
+    public void publisher_should_have_only_books_namely_and_Rutuja(String publisherName, int expBooksCountForPublisher, String expBookTitle1,
             String expBookTitle2){
 
         List<String> expBookTitleList = Arrays.asList(expBookTitle1,expBookTitle2);
@@ -162,8 +161,8 @@ public class ValidateGetResponse {
                         (actBookTitleList.containsAll(expBookTitleList)) && (expBookTitleList.containsAll(actBookTitleList)));
 }
 	
-    @Then("{string} publisher is not the publishing any book for author {string}")
-    public void publisher_is_not_the_publishing_any_book_for_author(String publisherToCheck, String authorToCheckAgainst){
+    @Then("{string} publisher is not the publishing any book for author_Rutuja {string}")
+    public void publisher_is_not_the_publishing_any_book_for_author_Rutuja(String publisherToCheck, String authorToCheckAgainst){
 
                 List<String> authorList = new ArrayList<String>();
                 List<Map<String, Object>> booksJsonList = response.body().jsonPath().getList("books");
@@ -181,8 +180,8 @@ public class ValidateGetResponse {
     }
 
 
-    @Then("{string} publisher should have exactly {int} books")
-    public void publisher_should_have_exactly_books(String publisherName, Integer expCountOfBooksForPublisher){
+    @Then("{string} publisher should have exactly {int} books_Rutuja")
+    public void publisher_should_have_exactly_books_Rutuja(String publisherName, Integer expCountOfBooksForPublisher){
 
                 int actCountOfBooksForPublisher = 0;
                 List<Map<String, Object>> booksJsonList = response.body().jsonPath().getList("books");
