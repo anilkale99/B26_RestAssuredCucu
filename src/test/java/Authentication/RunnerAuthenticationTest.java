5853fb81cbcf1b420967c48a6fb77735a1284284
Anil Kale <anilkale.99@gmail.com>
2025-04-26 11:04:39 +0530

    authorization code

---
+++ src/test/java/Authentication/RunnerAuthenticationTest.java
@@ -0,0 +1,27 @@
+package Authentication;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.platform.engine.Constants;
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("Authentication")
+@ConfigurationParameter(key = Constants.FEATURES_PROPERTY_NAME,value = "src/test/resources/Authentication/")
+@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME,value = "Authentication")
+//@ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME,value = "@googleSearch")
+//@ConfigurationParameter(key = Constants.EXECUTION_DRY_RUN_PROPERTY_NAME,value = "false")
+@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME,value = "pretty, html:target/cucumber-report/cucumber.html")
+@ConfigurationParameter(key = Constants.PARALLEL_CONFIG_FIXED_PARALLELISM_PROPERTY_NAME,value = "1")
+@ConfigurationParameter(key = Constants.PARALLEL_CONFIG_STRATEGY_PROPERTY_NAME,value = "fixed")
+@ConfigurationParameter(key = Constants.PARALLEL_EXECUTION_ENABLED_PROPERTY_NAME,value = "true")
+
+
+public class RunnerAuthenticationTest {
+	
+	
+}
