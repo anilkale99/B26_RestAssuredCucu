de231fe8f51a02b810a5c05447f21478eeb71c16
unknown <prasad.more.jgd@gmail.com>
2025-05-30 22:03:23 -0700

    fixed runner file by replacing html with json for json report

--- src/test/java/get_headers/RunnerForGetHeadersTest.java
+++ src/test/java/get_headers/RunnerForGetHeadersTest.java
@@ -13,7 +13,7 @@ import io.cucumber.junit.platform.engine.Constants;
 @ConfigurationParameter(key = Constants.FEATURES_PROPERTY_NAME,value = "src/test/resources/get_headers/GetHeadersTesting.feature")
 @ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME,value = "get_headers")
 @ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME,value = "pretty, html:target/cucumber-report/cucumber.html")
-@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME,value = "pretty, html:target/cucumber-report/Report5.json")
+@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME,value = "pretty, json:target/cucumber-report/Report5.json")
 @ConfigurationParameter(key = Constants.PARALLEL_CONFIG_FIXED_PARALLELISM_PROPERTY_NAME,value = "1")
 @ConfigurationParameter(key = Constants.PARALLEL_CONFIG_STRATEGY_PROPERTY_NAME,value = "fixed")
 @ConfigurationParameter(key = Constants.PARALLEL_EXECUTION_ENABLED_PROPERTY_NAME,value = "false")
--- src/test/java/prasad_runner/runnerFile_March22_AssignmentTest.java
+++ src/test/java/prasad_runner/runnerFile_March22_AssignmentTest.java
@@ -14,7 +14,7 @@ import io.cucumber.junit.platform.engine.Constants;
 //@ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME,value = "@Regression or Sanity")
 @ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME,value = "prasad_SD")
 @ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME,value = "pretty, html:target/cucumber-report/cucumber.html")
-//@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME,value = "pretty, html:target/cucumber-report/runnerFile_March22_AssignmentTest.json")
+@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME,value = "pretty, json:target/cucumber-report/runnerFile_March22_AssignmentTest.json")
 @ConfigurationParameter(key = Constants.PARALLEL_CONFIG_FIXED_PARALLELISM_PROPERTY_NAME,value = "2")
 @ConfigurationParameter(key = Constants.PARALLEL_CONFIG_STRATEGY_PROPERTY_NAME,value = "fixed")
 @ConfigurationParameter(key = Constants.PARALLEL_EXECUTION_ENABLED_PROPERTY_NAME,value = "true")
